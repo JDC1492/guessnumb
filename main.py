546ccb372a61c15455c7e87adbc74355ef7bad9f
Jonathan Cohen <65975460+JDC1492@users.noreply.github.com>
2021-12-07 02:09:58 +0000

    guesses work correctly.

--- main.py
+++ main.py
@@ -8,3 +8,20 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
+import random
+
+selected_number = random.randint(1, 100)
+is_game_over = False
+print("Welcome to the Guessing Game.")
+
+while not is_game_over:
+  plr_guess = int(input("Guess a number between 1-100:\n"))
+  if plr_guess < selected_number:
+    print("Too Low")
+  elif plr_guess > selected_number:
+    print("Too High")
+  elif plr_guess == selected_number:
+    is_game_over = True
+    print(f"Correct!! The selected_number was: {selected_number}")
+
+
